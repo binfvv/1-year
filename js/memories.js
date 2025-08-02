30e4f3899d3344098049251e248ab316c97e6477
bot <doanducbinh07101990@gmail.com>
2025-08-03 00:23:51 +0700

    fix lần 1

--- js/memories.js
+++ js/memories.js
@@ -1,15 +1,15 @@
 const imageList = [
-  "/images/photo1.jpg",
-  "/images/photo2.jpg",
-  "/images/photo3.jpg",
-  "/images/photo4.jpg",
-  "/images/photo5.jpg",
-  "/images/photo6.jpg",
-  "/images/photo7.jpg",
-  "/images/photo8.jpg",
-  "/images/photo9.jpg",
-  "/images/photo10.jpg",
-  "/images/photo11.jpg"
+  "images/photo1.jpg",
+  "images/photo2.jpg",
+  "images/photo3.jpg",
+  "images/photo4.jpg",
+  "images/photo5.jpg",
+  "images/photo6.jpg",
+  "images/photo7.jpg",
+  "images/photo8.jpg",
+  "images/photo9.jpg",
+  "images/photo10.jpg",
+  "images/photo11.jpg",
 ];
 
 let currentIndex = 0;
